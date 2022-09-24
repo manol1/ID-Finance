c8b3fdbda2e437ec08e95905385a7c903eb4bbc0
manol1 <marsen1491@gmail.com>
2022-09-25 01:49:23 +0300

    feat: add breadcrumbs, mui

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import styles from "./App.module.scss";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { MainPage } from "./pages/MainPage";
-import styles from "./App.module.scss";
+import { BreadСrumbs } from "./components/BreadСrumbs";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <BreadСrumbs />
       <MainPage />
       <Footer />
     </div>
---
+++ test/src/components/BreadСrumbs/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+export function BreadСrumbs() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>...SignUpInfo/</div>
+    </div>
+  );
+}
